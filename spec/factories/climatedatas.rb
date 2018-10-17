adbc59ef7bdaa0fc205c63eca801acca8745ce09
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2018-10-17 23:17:42 +0100

    write factory files for each model - create fake instances of the models for use in testing

---
+++ spec/factories/climatedatas.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :climatedata do
+    max_temp { Faker::Number.decimal(2, 1) }
+    min_temp { Faker::Number.decimal(2, 1) }
+    mean_temp { Faker::Number.decimal(2, 1) }
+    total_rainfall { Faker::Number.decimal(2, 1) }
+    total_sunshine { Faker::Number.decimal(2, 1) }
+    month { Faker::Number.between(1, 12) }
+    year { Faker::Number.between(1, 10000) }
+    weatherstation_id nil
+  end
+end

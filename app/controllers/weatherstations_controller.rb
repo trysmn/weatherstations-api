a8ca7a0fabf75e41337537af1d115ce843b54aa1
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2018-10-18 09:44:49 +0100

    define all methods in the WeatherstationsController for CRUD for the weatherstations and define helper modules in the response directory used in the methods (included in ApplicationController)

--- app/controllers/weatherstations_controller.rb
+++ app/controllers/weatherstations_controller.rb
@@ -1,2 +1,43 @@
 class WeatherstationsController < ApplicationController
+  before_action :set_weatherstation, only: [:show, :update, :destroy]
+
+  # GET /api/weatherstations
+  def index
+    @weatherstations = Weatherstation.all
+    json_response(@weatherstations)
+  end
+
+  # POST /api/weatherstations
+  def create
+    @weatherstation = Weatherstation.create!(weatherstation_params)
+    json_response(@weatherstation, :created)
+  end
+
+  # GET /api/weatherstations/:id
+  def show
+    json_response(@weatherstation)
+  end
+
+  # PUT /api/weatherstations/:id
+  def update
+    @weatherstation.update(weatherstation_params)
+    head :no_content
+  end
+
+  # DELETE /api/weatherstations/:id
+  def destroy
+    @weatherstation.destroy
+    head :no_content
+  end
+
+  private
+
+  def weatherstation_params
+    # whitelist params
+    params.permit(:name)
+  end
+
+  def set_weatherstation
+    @weatherstation = Weatherstation.find(params[:id])
+  end
 end
